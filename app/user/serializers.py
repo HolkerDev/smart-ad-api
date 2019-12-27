a6de080608af6428a11134d03d6f05b1ced78357
Mykola Penkovskyi <mykola.penkovskyi@snpgroup.com>
2019-12-27 19:04:21 +0100

    Add token and user management

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -6,7 +6,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     """Serializer for the user model"""
 
-    class Meta():
+    class Meta:
         model = get_user_model()
         fields = ('email', 'password', 'name')
         extra_kwargs = {
@@ -16,6 +16,16 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create a new user with encrypted password and return it"""
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update an existing user and return it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
 
 
 class AuthTokenSerializer(serializers.Serializer):
